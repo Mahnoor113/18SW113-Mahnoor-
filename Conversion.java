29182848fa4861d194183c12f1c4799485fa12db
Mahnoor113 <49373269+Mahnoor113@users.noreply.github.com>
2019-04-07 11:17:48 -0700

    lab no1 tasks

---
+++ Conversion.java
@@ -0,0 +1,11 @@
+class Conversion{
+
+public static void main(String args[]){
+
+double a=50.25;
+int b=(int)a;
+double c= a-b;
+System.out.println(a);
+System.out.println(b);
+}
+}
---
+++ Report.java
@@ -0,0 +1,21 @@
+class Report{
+public static void main(String args[]){
+
+String Name;
+Name="Mahnoor";
+int Marks;
+Marks=966;
+double Percentage;
+Percentage=88;
+char Grade;
+Grade='A';
+boolean Status;
+Status=true;
+System.out.println(Name);
+System.out.println(Marks);
+System.out.println(Percentage);
+System.out.println(Grade);
+System.out.println(Status);
+}
+
+}
